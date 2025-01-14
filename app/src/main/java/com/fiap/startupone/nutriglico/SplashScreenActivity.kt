091814a1c102467142d9ef622a4092490f9744ec
Renan <reamco@gmail.com>
2025-01-14 12:37:57 -0300

    SplashScreen

--- app/src/main/java/com/fiap/startupone/nutriglico/SplashScreenActivity.kt
+++ app/src/main/java/com/fiap/startupone/nutriglico/SplashScreenActivity.kt
@@ -1,12 +1,14 @@
 package com.fiap.startupone.nutriglico
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.fiap.startupone.nutriglico.features.auth.ui.AuthActivity
 
-class MainActivity : ComponentActivity() {
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         // Instala a SplashScreen
         installSplashScreen()
