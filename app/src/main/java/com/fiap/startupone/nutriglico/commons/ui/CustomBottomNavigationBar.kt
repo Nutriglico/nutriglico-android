04739c72da7395a9eae4668f80217b7c31c900ca
Renan <reamco@gmail.com>
2025-01-14 15:02:24 -0300

    Implementar estratégia single top no Navigation bar

--- app/src/main/java/com/fiap/startupone/nutriglico/commons/ui/CustomBottomNavigationBar.kt
+++ app/src/main/java/com/fiap/startupone/nutriglico/commons/ui/CustomBottomNavigationBar.kt
@@ -8,8 +8,10 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.fiap.startupone.nutriglico.R
 
 @Composable
@@ -18,8 +20,16 @@ fun BottomNavigationBar(navController: NavController) {
         NavigationBarItem(
             icon = { Icon(Icons.Default.Home, contentDescription = "Home") },
             label = { Text("Home") },
-            selected = false,
-            onClick = { navController.navigate("home") }
+            selected = currentRoute(navController) == "home",
+            onClick = {
+                navController.navigate("home") {
+                    popUpTo(navController.graph.startDestinationId) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
         )
         NavigationBarItem(
             icon = {
@@ -29,14 +39,30 @@ fun BottomNavigationBar(navController: NavController) {
                 )
             },
             label = { Text("Glicemia") },
-            selected = false,
-            onClick = { navController.navigate("glicemia") }
+            selected = currentRoute(navController) == "glicemia",
+            onClick = {
+                navController.navigate("glicemia") {
+                    popUpTo(navController.graph.startDestinationId) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
         )
         NavigationBarItem(
             icon = { Icon(Icons.Default.Add, contentDescription = "Inserir") },
             label = { Text("Inserir") },
-            selected = false,
-            onClick = { navController.navigate("inserir") }
+            selected = currentRoute(navController) == "inserir",
+            onClick = {
+                navController.navigate("inserir") {
+                    popUpTo(navController.graph.startDestinationId) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
         )
         NavigationBarItem(
             icon = {
@@ -46,14 +72,36 @@ fun BottomNavigationBar(navController: NavController) {
                 )
             },
             label = { Text("Refeição") },
-            selected = false,
-            onClick = { navController.navigate("refeicao") }
+            selected = currentRoute(navController) == "refeicao",
+            onClick = {
+                navController.navigate("refeicao") {
+                    popUpTo(navController.graph.startDestinationId) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
         )
         NavigationBarItem(
             icon = { Icon(painterResource(id = R.drawable.ic_menu), contentDescription = "Menu") },
             label = { Text("Menu") },
-            selected = false,
-            onClick = { navController.navigate("menu") }
+            selected = currentRoute(navController) == "menu",
+            onClick = {
+                navController.navigate("menu") {
+                    popUpTo(navController.graph.startDestinationId) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
         )
     }
+}
+
+@Composable
+fun currentRoute(navController: NavController): String? {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    return navBackStackEntry?.destination?.route
 }
