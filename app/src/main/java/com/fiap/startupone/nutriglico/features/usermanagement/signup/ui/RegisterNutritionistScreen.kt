d1da2911b523b7e110861a28159da5b676bc8995
Renan <reamco@gmail.com>
2025-01-16 19:53:52 -0300

    Scrollview na tela de cadastro

--- app/src/main/java/com/fiap/startupone/nutriglico/features/usermanagement/signup/ui/RegisterNutritionistScreen.kt
+++ app/src/main/java/com/fiap/startupone/nutriglico/features/usermanagement/signup/ui/RegisterNutritionistScreen.kt
@@ -1,7 +1,13 @@
 package com.fiap.startupone.nutriglico.features.usermanagement.signup.ui
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -9,7 +15,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -36,9 +41,9 @@ fun RegisterNutritionistScreen(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(paddingValues)
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Top
             ) {
                 OutlinedTextField(
                     value = name.value,
